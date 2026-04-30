1deea3444a9d2b1cc4c086b46e8636022021e90d
zen4215 <44k1.373@gmail.com>
2026-05-01 00:54:41 +0900

    change: named struct

--- include/letter_can.hpp
+++ include/letter_can.hpp
@@ -11,20 +11,8 @@ union Identifier {
         uint32_t target: 8;
         uint32_t source: 8;
         uint32_t unused: 13;
-    };
+    } persed;
 };
-
-// struct Identifier {
-//     Identifier();
-//     Identifier(uint8_t sender_id, uint8_t receiver_id, uint16_t data);
-// 
-//     uint8_t  senderID   {0x00};
-//     uint8_t  receiverID {0x00};
-//     uint16_t data {0x0000};
-// 
-//     static uint32_t merge(Identifier id);
-//     static Identifier perse(uint32_t id);
-// };
 
 }
 }
